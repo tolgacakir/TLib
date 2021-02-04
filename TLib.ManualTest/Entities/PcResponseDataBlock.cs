ebd7e78dd40bdd72d03e067956a564d767a24f4e
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-04 13:21:15 +0300

    Add TLib.ManualTest

---
+++ TLib.ManualTest/Entities/PcResponseDataBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLib.ManualTest.Entities
+{
+    public class PcResponseDataBlock
+    {
+        public byte Order { get; set; }
+        public byte BoxId { get; set; }
+        public string Barcode { get; set; } = "";
+        public byte Response { get; set; }
+    }
+}
---
+++ TLib.ManualTest/Entities/PlcRequestDataBlock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLib.ManualTest.Entities
+{
+    public class PlcRequestDataBlock
+    {
+        public byte PlcRun { get; set; }
+        public PlcRequest[] PlcRequests { get; set; } = new PlcRequest[] { new PlcRequest() };
+    }
+
+    public class PlcRequest
+    {
+        public byte Order { get; set; }
+        public byte BoxId { get; set; }
+        public string Barcode { get; set; } = "";
+    }
+}
