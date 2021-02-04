8be01e478c363a8b5fe7b823cb12fbbf2f83237c
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-04 12:55:31 +0300

    Add DeviceNoConnectionException

---
+++ TLib.Device/Exceptions/DeviceNoConnectionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLib.Device.Plc.Exceptions
+{
+    public class DeviceNoConnectionException : Exception
+    {
+        public DeviceNoConnectionException(string message="The device connection is NOT OK") : base(message)
+        {
+
+        }
+    }
+}
