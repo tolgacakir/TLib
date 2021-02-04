8a680febd3096dea9fba6c97e4aa83b6554921e9
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-04 12:56:05 +0300

    Add S7PlcClient

---
+++ TLib.Device/Plc/Concrete/Sharp7/S7PlcClient.cs
@@ -0,0 +1,104 @@
+﻿using Sharp7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLib.Device.Plc.Abstract;
+using TLib.Device.Plc.Concrete.AutoConnector;
+using TLib.Device.Plc.Exceptions;
+
+namespace TLib.Device.Plc.Concrete.Sharp7
+{
+    public abstract class S7PlcClient : IPlcClient
+    {
+        public string IpAddress { get; }
+        public int Rack { get; }
+        public int Slot { get; }
+        public bool ConnectionStatus => _s7Client.Connected;
+
+        protected const int NO_ERROR = 0;
+        protected readonly S7Client _s7Client;
+        protected readonly PlcAutoConnector _plcReconnector;
+        protected S7PlcClient(string ipAddress, int rack=0, int slot=1, bool autoReconnect=true)
+        {
+            IpAddress = ipAddress;
+            Rack = rack;
+            Slot = slot;
+
+            _s7Client = new S7Client();
+            _s7Client.ConnTimeout = 1000;
+            _s7Client.RecvTimeout = 500;
+            _s7Client.SendTimeout = 500;
+
+            if (autoReconnect)
+            {
+                _plcReconnector = new PlcAutoConnector(this);
+            }
+        }
+
+        public bool Connect()
+        {
+            return _s7Client.ConnectTo(IpAddress,Rack,Slot) == NO_ERROR;
+        }
+
+        public void Disconnect()
+        {
+            _s7Client.Disconnect();
+        }
+
+        protected bool Get(int dbNumber, int start, ref byte[] buffer)
+        {
+            if (ConnectionStatus)
+            {
+                var result = _s7Client.DBRead(dbNumber, start, buffer.Length, buffer);
+                if (result == NO_ERROR)
+                {
+                    return true;
+                }
+                else if (result == S7Consts.errTCPNotConnected)
+                {
+                    //TODO: no connection hatası. bu hatadan sonra tekrar bağlanma mekanizması çalıştırılmalı.
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    //TODO: burada okuma hatası.
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                throw new DeviceNoConnectionException($"Data reading is not completed. S7 Plc has not connection. Ip: {IpAddress}");
+            }
+            
+        }
+
+        protected bool Set(int dbNumber, int start, in byte[] buffer)
+        {
+            if (ConnectionStatus)
+            {
+                var result = _s7Client.DBWrite(dbNumber, start, buffer.Length, buffer);
+                if (result == NO_ERROR)
+                {
+                    return true;
+                }
+                else if (result == S7Consts.errTCPNotConnected)
+                {
+                    //TODO: no connection hatası. bu hatadan sonra tekrar bağlanma mekanizması çalıştırılmalı.
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    //TODO: burada okuma hatası.
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                throw new DeviceNoConnectionException($"Data writing is not completed. S7 Plc has not connection. Ip: {IpAddress}");
+            }
+            
+        }
+    }
+}
