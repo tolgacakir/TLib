ad4b075151c40c0e69a8f7b8564d87099fb12a45
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-04 11:51:42 +0300

    Add PlcAutoConnector

---
+++ TLib.Device/Plc/Concrete/AutoConnector/PlcAutoconnector.cs
@@ -0,0 +1,33 @@
+﻿using System.Timers;
+using TLib.Device.Plc.Abstract;
+
+namespace TLib.Device.Plc.Concrete.AutoConnector
+{
+    //TODO: sadece plc için değil, tüm device'lar için kullanılabilir.
+    public class PlcAutoConnector
+    {
+        private readonly IPlcClient _plcClient;
+        private readonly Timer _timer;
+
+        public PlcAutoConnector(IPlcClient plcClient, double interval = 3000.0)
+        {
+            _plcClient = plcClient;
+            _timer = new Timer(interval);
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //TODO: asenkron erişimde sorun çıkıyor. bu yüzden timer durdurulup tekrar başlatılıyor. farklı şekilde çözülebilir.
+            _timer.Stop();
+
+            if (! _plcClient.ConnectionStatus)
+            {
+                _plcClient.Connect();
+            }
+
+            _timer.Start();
+        }
+    }
+}
