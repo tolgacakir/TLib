8d4e1869d80a91334e04658761f04f8f705f4f1f
Tolga Çakır <cakrtolga13@gmail.com>
2021-02-04 13:34:04 +0300

    Add TLib.ManualTest.DataAccessLayer

---
+++ TLib.ManualTest/DataAccessLayer/Abstract/IDummyPlcClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLib.Device.Plc.Abstract;
+using TLib.ManualTest.Entities;
+
+namespace TLib.ManualTest.DataAccessLayer.Abstract
+{
+    public interface IDummyPlcClient : IPlcClient
+    {
+        bool GetPlcRequest(out PlcRequestDataBlock plcRequest);
+        bool SetPcResponse(in PcResponseDataBlock pcResponse);
+    }
+}
---
+++ TLib.ManualTest/DataAccessLayer/Concrete/Json/JsonDummyPlcClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLib.Device.Plc.Concrete.Json;
+using TLib.ManualTest.DataAccessLayer.Abstract;
+using TLib.ManualTest.Entities;
+
+namespace TLib.ManualTest.DataAccessLayer.Concrete.Json
+{
+    public class JsonDummyPlcClient : JsonPlcClient<PlcRequestDataBlock,PcResponseDataBlock>, IDummyPlcClient
+    {
+        public JsonDummyPlcClient(int id = 1) : base(id)
+        {
+
+        }
+
+        public bool GetPlcRequest(out PlcRequestDataBlock plcRequest)
+        {
+            return Get(out plcRequest);
+        }
+
+        public bool SetPcResponse(in PcResponseDataBlock pcResponse)
+        {
+            return Set(in pcResponse);
+        }
+    }
+}
